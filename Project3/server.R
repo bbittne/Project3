142680e3b53fc979694c385cda06e63a9fcf755f
ndtaxresearch <109237028+ndtaxresearch@users.noreply.github.com>
2022-07-25 09:57:46 -0500

    Plot Changes

--- Project3/server.R
+++ Project3/server.R
@@ -8,19 +8,99 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(httr)
+library(jsonlite)
+library(caret)
+library(shinythemes)
+library(plotly)
+
+#Grab the data from the exported CSV file
+stockResults <- read_csv("../MSFTStockData.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    
+    #Data Exploration Section
+    output$DEPlot <- renderPlotly({
+        if(input$rdoPlotType == "histo"){
+            ##Histogram
+            histVar<-input$histoVariable
+            if (histVar=="o"){
+                xLabel="Opening Price"
+            }else if (histVar=="c") {
+                xLabel="Closing Price"
+            }else if (histVar=="vw") {
+                xLabel="Volume Weighted Avg Price"
+            }else {
+                xLabel="Aw Snap!"
+            }
+            x<-stockResults[[histVar]]
+            bins <- seq(min(x), max(x), length.out = input$bins + 1)
+            g <- ggplot(data = stockResults, aes(x = c))
+            ggplotly(g + geom_histogram(breaks = bins) +
+                         labs(title = paste0("Histogram of ",xLabel), x = xLabel, y = "Frequency"))
+        }else {
+            ##Scatter Plot
+            scatVar<-input$scatVariable
+            if (scatVar=="n"){
+                yLabel="Number of Transactions"
+            }else if (scatVar=="c") {
+                yLabel="Closing Price"
+            }else if (scatVar=="v") {
+                yLabel="Trading Volume"
+            }else {
+                yLabel="Aw Snap!"
+            }
+            y<-stockResults[[scatVar]]
+            g <- ggplot(data = stockResults, aes(x = tDate, y = y))
+            ggplotly(g + geom_point() +
+                         labs(title = paste0("Scatterplot for Trading Date and ", yLabel), x = "Trading Date", y = yLabel))
+        }
     })
+    
+
+    
+    # ##Scatter Plot
+    # output$scatterPlot <- renderPlotly({
+    #     scatVar<-input$scatVariable
+    #     if (scatVar=="n"){
+    #         yLabel="Number of Transactions"
+    #     }else if (scatVar=="c") {
+    #         yLabel="Closing Price"
+    #     }else if (scatVar=="v") {
+    #         yLabel="Trading Volume"
+    #     }else {
+    #         yLabel="Aw Snap!"
+    #     }
+    #     y<-stockResults[[scatVar]]
+    #     g <- ggplot(data = stockResults, aes(x = tDate, y = y))
+    #         ggplotly(g + geom_point() +
+    #             labs(title = paste0("Scatterplot for Trading Date and ", yLabel), x = "Trading Date", y = yLabel))
+    # })
+    # 
+    # ##Histogram
+    # output$histogramPlot <- renderPlotly({
+    #     histVar<-input$histoVariable
+    #     if (histVar=="o"){
+    #         xLabel="Opening Price"
+    #     }else if (histVar=="c") {
+    #         xLabel="Closing Price"
+    #     }else if (histVar=="vw") {
+    #         xLabel="Volume Weighted Avg Price"
+    #     }else {
+    #         xLabel="Aw Snap!"
+    #     }
+    #     x<-stockResults[[histVar]]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #     g <- ggplot(data = stockResults, aes(x = c))
+    #     ggplotly(g + geom_histogram(breaks = bins) +
+    #                  labs(title = paste0("Histogram of ",xLabel), x = xLabel, y = "Frequency"))
+    # })
+    
+
 
 })
--- Project3/ui.R
+++ Project3/ui.R
@@ -9,8 +9,15 @@
 
 library(shiny)
 library(tidyverse)
+library(dplyr)
+library(ggplot2)
 library(caret)
 library(shinythemes)
+library(plotly)
+
+#Grab the data from the exported CSV file
+stockResults <- read_csv("../MSFTStockData.csv")
+#print(stockResults)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(theme = shinytheme("cerulean"),
@@ -20,6 +27,8 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
         tabPanel("About",
             # Application title
             titlePanel("ST558 - Project 3"),
+            
+            h3("About"),
         
             # Sidebar with a slider input for number of bins
             sidebarLayout(
@@ -57,27 +66,68 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
                     img(src="MSLogo.png", height='225px',width='362px'),
                 )
             )
-        ),
+        ),#End About Section
+        
         tabPanel("Data Exploration",
              # Application title
              titlePanel("ST558 - Project 3"),
+             h3("Data Exploration"),
              
              # Sidebar with a slider input for number of bins
              sidebarLayout(
                  sidebarPanel(
-                     sliderInput("bins",
-                                 "Number of bins:",
-                                 min = 1,
-                                 max = 50,
-                                 value = 30)
+                     
+                     h4("Graphical Summaries"),
+                     
+                     radioButtons(inputId = "rdoPlotType", label = "Plot Type",
+                                  choiceValues = c("histo", "scatter" ),
+                                  choiceNames = c("Histogram", "Scatter Plot")),
+                         conditionalPanel("input.rdoPlotType == 'histo'",
+                            selectInput(inputId = "histoVariable", 
+                                      label = "Histogram Variable", 
+                                      choices = c("Opening Price" = "o",
+                                                  "Closing Price" = "c",
+                                                  "Volume Weighted Avg Price" = "vw")),
+                            sliderInput("bins",
+                                     "Number of bins:",
+                                     min = 1,
+                                     max = 50,
+                                     value = 30)
+                         ),#End Histogram Conditional Panel
+                        ##Scatterplot Conditional Panel
+                         conditionalPanel("input.rdoPlotType == 'scatter'",
+                                          selectInput(inputId = "scatVariable", 
+                                                      label = "Scatter Plot Variable", 
+                                                      choices = c("Trading Volume" = "v",
+                                                                  "Number of Transactions" = "n",
+                                                                  "Closing Price" = "c")),
+                         ),##End Scatter Plot Conditional Panel
+                     
+                     br(),
+                     h4("Numerical Summaries")
                  ),
                  
-                 # Show a plot of the generated distribution
+                 # # Show a plot of the generated distribution
                  mainPanel(
-                     plotOutput("distPlot")
+                     h3("Graphical Summaries"),
+                     plotlyOutput("DEPlot")
+                     #plotlyOutput("scatterPlot"),
+                     #plotlyOutput("histogramPlot")
                  )
+                 
+                 # mainPanel(h2("Graphical Summary"),
+                 #           plotlyOutput("scatterPlot"),
+                 #           br(),
+                 #           h2("Numerical Summaries"),
+                 #           dataTableOutput("exploreSummary"),
+                 #           h2("Data Set"),
+                 #           dataTableOutput("exploreData")
+                 # ) 
+                 
+                 
              )
-        ),
+        ),#End Data Exploration Section
+        
         tabPanel("Modeling",
              # Application title
              titlePanel("ST558 - Project 3"),
@@ -102,7 +152,8 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
                      )
                  )
              )
-        ),
+        ),#End Modeling Section
+        
         tabPanel("Data",
              # Application title
              titlePanel("ST558 - Project 3"),
@@ -122,6 +173,7 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
                      plotOutput("distPlot")
                  )
              )
-        )
+        )#End Data Section
+        
     )
 ))
